d21c043fc9af0adecbe7cb9ac225fad338b12116
Sojib Islam <sojibislam6505@gmail.com>
2026-04-19 20:43:21 +0600

    frontend deploy

--- src/hook/UseAxios.jsx
+++ src/hook/UseAxios.jsx
@@ -7,7 +7,7 @@ export default function UseAxios() {
 
   const axiosSecure = useMemo(() => {
     const instance = axios.create({
-      baseURL: "http://localhost:5000",
+      baseURL: import.meta.env.VITE_API_URL,
     });
 
     // request interceptor
